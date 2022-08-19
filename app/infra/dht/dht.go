49664af7b684d8dac7227c66ddbea45798caba36
HunterXuan <endpot@gmail.com>
2022-08-19 09:37:37 +0800

    fix: dht initial block

--- app/infra/dht/dht.go
+++ app/infra/dht/dht.go
@@ -18,23 +18,25 @@ func InitDHT() {
 
 	DHT = dht.NewWire(1024*8, 256, 16)
 
-	for resp := range DHT.Response() {
-		var info metainfo.Info
-		if err := bencode.Unmarshal(resp.MetadataInfo, info); err != nil {
-			log.Println("unmarshal info err:", err)
-			continue
-		}
+	go func() {
+		for resp := range DHT.Response() {
+			var info metainfo.Info
+			if err := bencode.Unmarshal(resp.MetadataInfo, info); err != nil {
+				log.Println("unmarshal info err:", err)
+				continue
+			}
 
-		if jsonInfo, err := json.Marshal(info); err != nil {
-			log.Println("marshal info err:", err)
-		} else if err := db.DB.Model(&model.Torrent{}).
-			Where("info_hash = ?", trackerUtil.RestoreToHexString(string(resp.InfoHash))).
-			Updates(map[string]interface{}{"meta_info": string(jsonInfo)}).Error; err != nil {
-			log.Println("update info err:", err)
-		} else {
-			log.Println("update info success:", jsonInfo)
+			if jsonInfo, err := json.Marshal(info); err != nil {
+				log.Println("marshal info err:", err)
+			} else if err := db.DB.Model(&model.Torrent{}).
+				Where("info_hash = ?", trackerUtil.RestoreToHexString(string(resp.InfoHash))).
+				Updates(map[string]interface{}{"meta_info": string(jsonInfo)}).Error; err != nil {
+				log.Println("update info err:", err)
+			} else {
+				log.Println("update info success:", jsonInfo)
+			}
 		}
-	}
+	}()
 
 	go DHT.Run()
 
