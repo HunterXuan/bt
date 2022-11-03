0dfe85fca66843b3ea6a47b7b7a4fff3bc7f0a1b
HunterXuan <endpot@gmail.com>
2022-11-03 08:41:46 +0800

    fix: shorten flush threshold

--- app/controller/job/cleaner.go
+++ app/controller/job/cleaner.go
@@ -82,7 +82,7 @@ func (s *Cleaner) Run() {
 			}
 		}
 
-		maximumValue := totalSystemMemory * 0.8
+		maximumValue := totalSystemMemory * 0.7
 		if usedMemory > 0 && totalSystemMemory > 0 && usedMemory > maximumValue {
 			log.Printf("since used memory (%v) exceed the maximum value (%v), start flushing...", usedMemory, maximumValue)
 			res, err := db.RDB.FlushDB(context.Background()).Result()
